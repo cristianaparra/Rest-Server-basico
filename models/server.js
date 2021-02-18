12364e34cf7e1c4e7c9301e57eccf7853b97acd2
Cristian Parra <cristian.a.parra@gmail.com>
2021-02-18 10:46:34 -0300

    importando mongoose y conectando con mongo data base

--- models/server.js
+++ models/server.js
@@ -1,17 +1,25 @@
 const express = require("express");
 const cors = require("cors");
 
+const { dbConnection } = require("../db/config");
 class Server {
   constructor() {
     this.app = express();
     this.port = process.env.PORT;
     this.usuariosPath = "/api/usuarios";
 
+    // conectar con base de dato
+    this.conectarDB();
+    
     // middlewares
     this.middlewares();
 
     // rutas de la app
     this.routes();
+  }
+
+  async conectarDB() {
+    await dbConnection();
   }
 
   middlewares() {
@@ -21,7 +29,7 @@ class Server {
 
     //pareso y lectura del body
     this.app.use(express.json());
-    
+
     //directorio publico
     this.app.use(express.static("public"));
   }

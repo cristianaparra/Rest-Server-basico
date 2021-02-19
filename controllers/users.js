d92fea1d471b765e050099797181607d9d2d5515
Cristian Parra <cristian.a.parra@gmail.com>
2021-02-19 13:04:11 -0300

    encriptando pass usando bcryptjs

--- controllers/users.js
+++ controllers/users.js
@@ -1,39 +1,52 @@
 const { response, request } = require("express");
-const Usuario = require('../models/usuario')
+const bcryptjs = require("bcryptjs");
+const Usuario = require("../models/usuario");
 
-const usuariosGet = (req= request, res = response) => {
-
-const {id, q} = req.query
+const usuariosGet = (req = request, res = response) => {
+  const { id, q } = req.query;
 
   res.json({
     msg: "get api - controlador",
     id,
-    q
+    q,
   });
 };
 const usuariosPost = async (req, res = response) => {
-  const body  = req.body;
-  const usuario = new Usuario(body);
+  const { nombre, correo, password, rol } = req.body;
+  const usuario = new Usuario({ nombre, correo, password, rol });
+
+  // verificar si correo existe
+
+  //encriptar la pass
+  let salt = bcryptjs.genSaltSync();
+  usuario.password = bcryptjs.hashSync(password, salt);
+
+  //guardar en BD
 
   await usuario.save();
 
   res.json({
-    msg: "post api - controlador",
-  usuario
+    usuario
   });
 };
+
+
 const usuariosPut = (req, res = response) => {
-    const {id} = req.params
+  const { id } = req.params;
   res.json({
     msg: "put api - controlador",
-    id
+    id,
   });
 };
+
+
 const usuariosPatch = (req, res = response) => {
   res.json({
     msg: "patch api - controlador",
   });
 };
+
+
 const usuariosDelete = (req, res = response) => {
   res.json({
     msg: "delete api - controlador",

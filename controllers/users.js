4309e2c19b9c1ba5ef331f79d3ae78296e66bc73
Cristian Parra <cristian.a.parra@gmail.com>
2021-02-25 12:23:08 -0300

    viendo el total de la coleccion y ademas haciendo una promesa que retorne las 2 respuestas  de la query

--- controllers/users.js
+++ controllers/users.js
@@ -3,14 +3,21 @@ const bcryptjs = require("bcryptjs");
 const Usuario = require("../models/usuario");
 
 const usuariosGet = async (req = request, res = response) => {
-  // const { id, q } = req.query;
+
+
+  const query = { estado: true };
   const { limite = 5, desde = 0 } = req.query;
-  const usuarios = await Usuario.find()
+
+  const resp = await Promise.all([
+    Usuario.countDocuments(query),
+    Usuario.find(query)
     .skip(Number(desde))
     .limit(Number(limite))
+    
+  ])
 
   res.json({
-    usuarios,
+   resp
   });
 };
 const usuariosPost = async (req, res = response) => {

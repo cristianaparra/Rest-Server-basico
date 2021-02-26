412b7a08f013255667c3595355b510c3054e09c0
Cristian Parra <cristian.a.parra@gmail.com>
2021-02-26 12:03:34 -0300

    validando password, correo, y estado en auth

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,16 +1,49 @@
-const{response}= require('express')
+const { response } = require("express");
+const Usuario = require("../models/usuario");
+const bcryptjs = require("bcryptjs");
+
+const login = async (req, res = response) => {
+  const { correo, password } = req.body;
+
+  try {
+    //verificar sai el email existe
+    const usuario = await Usuario.findOne({ correo });
+    if (!usuario) {
+      return res.status(400).json({
+        msg: "Usuario / Password no son correctos - correo",
+      });
+    }
+    //si el usuario esta activo
+    if (!usuario.estado) {
+      return res.status(400).json({
+        msg: "Usuario / Password no son correctos - estado:false",
+      });
+    }
+
+    //vetrificar la constraseña
+
+    const validarPassword = bcryptjs.compareSync(password, usuario.password);
+    if (!validarPassword) {
+      return res.status(400).json({
+        msg: "Usuario / Password no son correctos - pasword",
+      });
+    }
+
+    //generar jwt
 
 
+    
+    res.json({
+      msg: "Login Ok",
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      msg: "hable con el administrador",
+    });
+  }
+};
 
-const login = (req, res = response) => {
-
-  res.json({
-      msg: 'Login Ok'
-       } )
-  };
-  
- 
-  module.exports = {
-   login
-  };
-  
+module.exports = {
+  login,
+};
--- routes/auth.js
+++ routes/auth.js
@@ -3,17 +3,18 @@ const { check } = require("express-validator");
 
 const { validarCampos } = require("../middlewares/validar-campos");
 
-const{ login}= require('../controllers/auth');
+const { login } = require("../controllers/auth");
 
 const router = Router();
 
-router.post("/login",
-[
-    check('correo', 'el correo es obligatorio').isEmail(),
-    check('password', 'el contraseña es obligatorio').not().isEmpty(),
-    validarCampos
-] ,login);
-
-
+router.post(
+  "/login",
+  [
+    check("correo", "el correo es obligatorio").isEmail(),
+    check("password", "el contraseña es obligatorio").not().isEmpty(),
+    validarCampos,
+  ],
+  login
+);
 
 module.exports = router;

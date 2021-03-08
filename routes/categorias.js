cc530e084ae7e39b45c85c4613a1e69922b88184
Cristian Parra <cristian.a.parra@gmail.com>
2021-03-08 18:53:22 -0300

    creando models. categorias y index de los modelos para una exportacion mas ordenada

---
+++ routes/categorias.js
@@ -0,0 +1,36 @@
+const { Router } = require("express");
+const { check } = require("express-validator");
+
+const { validarCampos } = require("../middlewares/validar-campos");
+
+const router = Router();
+
+// {{url}}/api/categorias
+
+
+// obtener todas las categorias - publico
+router.get("/", (req, res) => {
+  res.json("get");
+});
+// obtener una categoria por id - publico
+router.get("/:id", (req, res) => {
+  res.json("get - ID");
+});
+
+// crear categoria - privado - cualquier persona con un token valido
+router.post("/", (req, res) => {
+  res.json("post");
+});
+
+// actualizar - privado - cualquier persona con un token valido
+router.put("/:id", (req, res) => {
+  res.json("put");
+});
+
+// borrar una categoria -Admin
+router.delete("/:id", (req, res) => {
+res.json("delete");
+});
+
+
+module.exports = router;
